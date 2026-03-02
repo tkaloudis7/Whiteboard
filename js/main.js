acaf46758dc223b292b49034570f66c7e4ac7136
Theodoros Kaloudis <theokaloudis7@gmail.com>
2026-03-03 00:35:19 +0200

    dark mode, bootstrap in form (task page)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
+//weather API
 document.addEventListener("DOMContentLoaded", () => {
 
   const tempElement = document.querySelector("#temp-display");
 
 
   if (!tempElement) return;
-
-
   fetch("https://api.open-meteo.com/v1/forecast?latitude=37.98&longitude=23.72&current_weather=true")
     .then(response => response.json())
     .then(data => {
@@ -19,3 +18,32 @@ document.addEventListener("DOMContentLoaded", () => {
       tempElement.innerText = "N/A";
     });
 });
+
+//dark mode button
+const toggleBtn = document.getElementById("darkModeToggle");
+
+//local storage check for previous data
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme === "dark") {
+  document.documentElement.setAttribute("data-bs-theme", "dark");
+  if (toggleBtn) toggleBtn.innerText = "Light Mode";
+}
+
+
+if (toggleBtn) {
+  toggleBtn.addEventListener("click", () => {
+    const htmlElement = document.documentElement;
+
+    if (htmlElement.getAttribute("data-bs-theme") === "dark") {
+      //change to light mode
+      htmlElement.setAttribute("data-bs-theme", "light");
+      localStorage.setItem("theme", "light");
+      toggleBtn.innerText = "Dark Mode";
+    } else {
+      //change to dark mode
+      htmlElement.setAttribute("data-bs-theme", "dark");
+      localStorage.setItem("theme", "dark");
+      toggleBtn.innerText = "Light Mode";
+    }
+  });
+}
